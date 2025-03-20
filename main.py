908f92591ef56475798208cf13c5ba2be522252a
wilsonm0135 <wilsonm0135@student.judsonisd.org>
2025-03-20 16:31:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+def main():
+  secret_number=random randit(1,100)
+  max_guesses = 7
+  num_guesses=0
+print("welcome to my number guessing game:3")
+print("im thinking of a number between 1 and 100. You have{max_guesses}guesses to guess it")
 
+
+while num_guesses < max_guesses:
+  guess= int(input("Enter your guess:'"))
+  num_guesses += 1 
