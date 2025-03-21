1b9f904e7182e0f2fb99bb75b9f94694475fc3d0
wilsonm0135 <wilsonm0135@student.judsonisd.org>
2025-03-21 16:20:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import random
+
 def main():
-  secret_number=random randit(1,100)
-  max_guesses = 7
-  num_guesses=0
-print("welcome to my number guessing game:3")
-print("im thinking of a number between 1 and 100. You have{max_guesses}guesses to guess it")
+    secret_number = random.randint(1,13)
+    max_guesses = 7
+    num_guesses=0
+    print("welcome to my number guessing game:3")
+    print("im thinking of a number between 1 and 13. You have{max_guesses}guesses to guess it")
+    
+    while num_guesses < max_guesses:
+      guess= int(input("Enter your guess:'"))
+      num_guesses += 1
+    
+    
+    if guess == secret_number:
+      print ("!!!congratulations! you guesses the right number in{num_guesses}guesses.")
+    
+    if guess < secret_number:
+      print("Too low! guess higher.")
+    else:
+      print("Too high guess lower.")
 
-
-while num_guesses < max_guesses:
-  guess= int(input("Enter your guess:'"))
-  num_guesses += 1 
+main()
